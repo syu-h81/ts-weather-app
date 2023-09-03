38a9c55b736ddd193891fd56b7e03fd6cb4097ae
Danjo Syu <>
2023-09-03 13:31:19 +0900

    feat: "ウェザーアプリ作成"

---
+++ src/components/Form.tsx
@@ -0,0 +1,17 @@
+//Form.tsx
+
+type FormPropstype = {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (e: any) => void;
+}
+
+const Form = (props: FormPropstype) => {
+  return (
+    <form>
+      <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+      <button type="submit" onClick={props.getWeather}>Get Weather</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+//Title.tsx
+
+const Title = () => {
+  return (
+    <h1>World Weather</h1>
+  );
+};
+
+export default Title;
